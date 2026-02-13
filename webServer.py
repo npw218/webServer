1c61e729bdbb30b21dc077e9187aeadd09ca7a82
npw218 <npw218@nyu.edu>
2026-02-12 19:47:06 -0500

    Update webServer.py

--- webServer.py
+++ webServer.py
@@ -65,7 +65,7 @@ def webServer(port=13331):
 
       #Close client socket
       #Fill in start
-      
+       connectionSocket.close()
       #Fill in end
 
   # Commenting out the below (some use it for local testing). It is not required for Gradescope, and some students have moved it erroneously in the While loop. 
@@ -75,6 +75,7 @@ def webServer(port=13331):
 
 if __name__ == "__main__":
   webServer(13331)
+
 
 
 
